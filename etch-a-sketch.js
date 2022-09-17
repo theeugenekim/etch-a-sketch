cdd4ecc3b849cf65d896500601a09e3aa560547c
Eugene Kim <theeugenekim@gmail.com>
2022-09-16 19:31:57 -0700

    Massively improve game UI/UX
    The game will now reset when clicking the button. Also, fixed an issue where the game area would duplicate after each click instead of resetting. This was fixed by clearing the grid-container node each time the button is clicked. The game will also start upon page load with a default value of 50 instead of waiting for the user to click the button.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,25 +1,62 @@
-const gridContainer = document.querySelector('.grid-container')
+const gridContainer = document.querySelector('.grid-container');
+const inputValue = document.querySelector('#input-box').value;
+const createGridButton = document.querySelector('.create-grid');
 
-function makeRows(rows, cols) {
+window.onload = buttonClick()
+
+function makeGrid(rows, cols) {
+    let width = 700;
+    let height = 700;
     gridContainer.style.setProperty('--grid-rows', rows);
     gridContainer.style.setProperty('--grid-cols', cols);
     for (i=0; i < (rows*cols); i++) {
         let cell = document.createElement('div');
-        cell.textContent = (i+1);
         gridContainer.appendChild(cell).className = ('grid-item');
+    };
+    gridContainer.style.cssText ="grid-template-columns: repeat(auto-fill, minmax("+ width/cols +"px, 1fr)); grid-template-rows: repeat(auto-fill, minmax("+height/rows+"px, 1fr));"
+    return gridContainer
+};
+
+function hover() {
+    const gridItem =  document.querySelectorAll('.grid-item');
+    createGridButton.addEventListener('click', buttonClick);
+
+    gridItem.forEach(item => {
+        item.addEventListener('mouseover', () => {
+            item.style.backgroundColor = 'red';
+        });
+    });
+}
+
+function validateInput(inputValue) {
+    if (inputValue < 1 || inputValue > 100) {
+        displayWarning()
+        return false
+    } else {
+        return true
     };
 };
 
+function displayWarning() {
+    let label = document.querySelector(".instructions").style.color = "red";
+    return true
+};
 
+function resetGame() {
+    // clear all DOM in grid-container node https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+    gridContainer.replaceChildren();
+}
 
+function buttonClick() {
+    const inputValue = document.querySelector('#input-box').value;
+    const validInput = validateInput(inputValue);
 
-makeRows(16,16)
+    resetGame()
 
-const gridItem =  document.querySelectorAll('.grid-item');
-
-
-gridItem.forEach(item => {
-    item.addEventListener('mouseover', () => {
-        item.style.backgroundColor = 'red';
-    });
-});
+    console.log(inputValue.value)
+    if (validInput === true) {
+        makeGrid(inputValue, inputValue);
+        console.log('hello');
+        hover();
+    };
+};
